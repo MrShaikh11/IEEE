d0103c773e202f170cec4c8b400c2a412ea8ead3
Sameer0708 <sameerpurnachandrasahu123@gmail.com>
2023-07-29 12:43:29 +0530

    color changes needed

--- contact-us.php
+++ contact-us.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <title>IEEE Website</title>
+    <title>Contact us</title>
     <link rel="stylesheet" href="include/style.css">
 </head>
 
